398ace7546ea985fd9bffbec5d08b312ed837299
Phytoplankton <katolarsen@gmail.com>
2025-12-28 18:08:25 +0100

    Change import statement to relative import
    Updated import statement to use relative import from main.py.

--- __init__.py
+++ __init__.py
@@ -3,7 +3,8 @@
 
 
 # Temporarily Fix
-from Repos.testlablib.main import *
+#from Repos.testlablib.main import *
+from .main import *  # Import everything from main.py
 #import Repos.testlablib.steady_state as steady_state
 #import Repos.testlablib.equilibrium as equilibrium
 #import Repos.testlablib.dynamic as dynamic
